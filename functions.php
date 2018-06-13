bd2d7d4da79c736489a9a03e1879acc8de6ca4f6
minhhung2202 <38975106+minhhung2202@users.noreply.github.com>
2018-06-13 10:51:32 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,5 +12,11 @@ function minhhung_load_theme_style() {
     }
     // Add Font Awesome
     wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );
+
+    // Owl Carousel
+    wp_enqueue_style( 'owl-min', get_stylesheet_directory_uri() . '/owl-carousel/owl.carousel.min.css', 'all' );
+    wp_enqueue_style( 'owl-theme', get_stylesheet_directory_uri() . '/owl-carousel/owl.theme.default.min.css', 'all' );
+    wp_enqueue_script( 'owl-min-js', get_stylesheet_directory_uri() . '/owl-carousel/jquery.min.js', 'all' );
+    wp_enqueue_script( 'owl-carousel-js', get_stylesheet_directory_uri() . '/owl-carousel/owl.carousel.js', 'all' );
 }
 add_action('wp_enqueue_scripts', 'minhhung_load_theme_style', 909 );
